e8d46705a8d306642c9177fae3178bed19200732
Sarah Estes <sestes2@student.cscc.edu>
2026-09-17 21:26:10 -0400

    Added area and perimeter functions

--- rectangle.py
+++ rectangle.py
@@ -3,3 +3,12 @@
     Rectangle Area and Permeter Functions
     9/17/2026"""
 
+def calc_area(width, height):
+    """Calculates and returns the area of a rectangle"""
+    rectangle_area = width * height
+    return rectangle_area
+
+def calc_perimeter(width, height):
+    """Calculates and returns the perimeter of a rectangle"""
+    rectangle_perimeter = 2 * (width + height)
+    return rectangle_perimeter
