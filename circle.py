43eb55d313792a8b25888f5870b48e7a1e7e26ad
Sarah Estes <sestes2@student.cscc.edu>
2026-09-17 21:21:15 -0400

    Updated comments to docstrings under functions

--- circle.py
+++ circle.py
@@ -7,11 +7,11 @@
 import math
 
 def calc_area(radius):
-    # calculates and returns the area
+    """Calculates and returns the area of a circle"""
     circle_area = math.pi(radius ** 2)
     return circle_area
 
 def calc_circumference(radius):
-    # calculates and returns the circumference
+    """Calculates and returns the circumference of a circle"""
     circle_circumference = 2 * math.pi * radius
     return circle_circumference
