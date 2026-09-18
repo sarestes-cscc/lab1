d30af7e7d981329f37e7eddf80a6a366fa1918ef
Sarah Estes <sestes2@student.cscc.edu>
2026-09-17 21:18:19 -0400

    Imported Python Math Module Added circle area and circumference functions

--- circle.py
+++ circle.py
@@ -1,5 +1,17 @@
 """ Geometry Calculator
     Sarah Estes
     Circle Area and Circumference functions
+    Using the built-in Python Math module
     9/17/2026"""
 
+import math
+
+def calc_area(radius):
+    # calculates and returns the area
+    circle_area = math.pi(radius ** 2)
+    return circle_area
+
+def calc_circumference(radius):
+    # calculates and returns the circumference
+    circle_circumference = 2 * math.pi * radius
+    return circle_circumference
