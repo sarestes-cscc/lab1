d6c195f19ddb5eac874d1b27b608e75854607dee
Sarah Estes <sestes2@student.cscc.edu>
2026-09-17 21:06:45 -0400

    Added beginning of while loop with print statements

--- Lab8_sestes2-2.py
+++ Lab8_sestes2-2.py
@@ -4,3 +4,14 @@
     and rectangle area & perimeter
     9/17/2026"""
 
+user_choice = ""
+
+while user_choice:
+    print("Geometry Calculator")
+    print("-------------------")
+    print("1. Calculate Circle Area")
+    print("2. Calculate Circle Circumference")
+    print("3. Calculate Rectangle Area")
+    print("4. Calculate Rectangle Perimeter")
+    print("5. Exit")
+    user_choice = input("Enter your choice (1-5): ")
